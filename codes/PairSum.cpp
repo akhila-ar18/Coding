1146e6103c42dd0eadf9677d5f4e14f90f95b546
AKHILA A R <107562708+akhila-ar18@users.noreply.github.com>
2022-09-08 23:13:19 +0530

    added

---
+++ codes/PairSum.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define f first
+#define s second
+#define ll long long int
+using namespace std;
+
+void solve(string &s)
+{
+    stack<char> st;
+    string str;
+    for(int i=0;i<s.size();i++)
+    {
+        if(st.empty() || st.top()!=s[i])
+        {
+            st.push(s[i]);
+        }
+        else
+        {
+            st.push('*');
+            st.push(s[i]);
+        }
+
+    }
+    while(!st.empty())
+    {
+        str.pb(st.top());
+        st.pop();
+    }
+    reverse(str.begin(),str.end());
+    cout<<str;
+}
+int main() {
+    int r;
+    string s="aabbcc";
+    solve(s);
+    return 0;
+}
